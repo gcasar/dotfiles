352ede55d1506f2b8f9f7685d1200850af67a8e0
Gregor Casar <gregorcasar@gmail.com>
2021-06-06 11:08:19 +0200

    Remove `cask` from brew commands

--- setup.sh
+++ setup.sh
@@ -7,22 +7,22 @@ xcode-select —-install
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # general applications
-brew cask install slack
-brew cask install lastpass
-brew cask install firefox
-brew cask install google-chrome
+brew install slack
+brew install lastpass
+brew install firefox
+brew install google-chrome
 
 # terminal
-brew cask install iterm2
+brew install iterm2
 brew install zsh
 #oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 upgrade_oh_my_zsh
 
 # development applications
-brew cask install docker
-brew cask install pycharm
-brew cask install visual-studio-code
+brew install docker
+brew install pycharm
+brew install visual-studio-code
 
 brew install python3
 
